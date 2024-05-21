2122b4d7c82ead74b96dbedfcd632e2f1ec8b03d
milula022 <milicc016@gmail.com>
2024-05-21 10:56:03 -0300

    configuracion del modulo de autentificacion y sus rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,9 @@ const routes: Routes = [
   },
   {
     path:"",loadChildren:()=>import('./modules/producto/producto.module').then(m=>m.ProductoModule)
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/autentificacion/autentificacion.module').then(m=>m.AutentificacionModule)
   }
 ];
 
