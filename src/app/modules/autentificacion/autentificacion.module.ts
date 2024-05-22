8328a5ed50559b00b30de5c1551785adc1a562eb
milula022 <milicc016@gmail.com>
2024-05-22 09:55:21 -0300

    configuramos formulario

--- src/app/modules/autentificacion/autentificacion.module.ts
+++ src/app/modules/autentificacion/autentificacion.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+//vistas de autentificacion
 import { AutentificacionRoutingModule } from './autentificacion-routing.module';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
 
+//componentes de material
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,11 +19,19 @@ import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.compone
   ],
   imports: [
     CommonModule,
-    AutentificacionRoutingModule
+    AutentificacionRoutingModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   exports:[
     RegistroComponent,
-    IniciosesionComponent
+    IniciosesionComponent,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
 
   ]
 })
