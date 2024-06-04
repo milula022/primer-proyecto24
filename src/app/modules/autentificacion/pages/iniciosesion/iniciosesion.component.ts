f29d1fb6c1b697e76cfd49ddf5cba8e453cbca69
milula022 <milicc016@gmail.com>
2024-06-04 10:55:35 -0300

    avanzando con la consigna aulica

--- src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
+++ src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-iniciosesion',
@@ -8,4 +9,79 @@ import { Component } from '@angular/core';
 export class IniciosesionComponent {
   hide = true;
 
+  public datos: Usuario[];
+  constructor() {
+    this.datos = [
+      {
+        uid: "12323",
+        nombre: "juab",
+        apellido: "perez",
+        email: "juabperez@gmail.com",
+        password: "12345",
+        rol: "visitante"
+      },
+      {
+        uid: "12344",
+        nombre: "franco",
+        apellido: "cifuentes",
+        email: "francocifuentes@gmail.com",
+        password: "12345",
+        rol: "visitante"
+      },
+      {
+        uid: "12345",
+        nombre: "emilia",
+        apellido: "perez",
+        email: "emiliaperez@gmail.com",
+        password: "12345",
+        rol: "admi"
+      }
+
+    ]
+  }
+
+  usuarios: Usuario = {
+    uid: '',//inicializamos con comilla simple porque es de STRING
+    nombre: '',
+    apellido: '',
+    email: '',
+    password: '',
+    rol: ''
+
+  }
+  //creamos coleccion de usuarios, tipo 'usuario' para arrays
+  coleccionUsuarios: Usuario[] = [];
+
+  comparar() {
+    //  const users = {
+    // uid: this.iniciar.uid,
+    //  nombre: this.iniciar.nombre,
+    //  apellido: this.iniciar.apellido,
+    //  email: this.iniciar.email,
+    //  password: this.iniciar.password,
+    //  rol: this.iniciar.rol
+    const credenciales = {
+      uid: this.usuarios.uid,
+      nombre: this.usuarios.nombre,
+      apellido: this.usuarios.apellido,
+      email: this.usuarios.email,
+      password: this.usuarios.password,
+      rol: this.usuarios.rol
+    }
+
+    for (let i = 0; i <= this.datos.length; i++) {
+      if (this.datos.length !== (credenciales.uid && credenciales.nombre && credenciales.apellido && credenciales.email && credenciales.password && credenciales.rol)) {
+        alert("inicio sesidada")
+        break
+      }
+      else {
+        alert("no iniciaste sesion")
+        break
+      }
+    }
+
+
+  }
+
 }
+
