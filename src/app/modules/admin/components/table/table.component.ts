92aad757648a2bc36804e46248ca14107650f4d7
milula022 <milicc016@gmail.com>
2024-08-05 10:59:42 -0300

    creacion de metodo 'agregarProducto' desde table.ts y creacion del modal desde el html

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -19,16 +19,41 @@ export class TableComponent {
   //definimos formulario para los productos
   //(atributos alfanumericos (string) se inicializan con comillas simples)
   //(atributos numericos (number) se inicializan con cero('0'))
-  producto=new FormGroup({
-    nombre:new FormControl('',Validators.required),
-    precio:new FormControl(0,Validators.required),
-    descripcion:new FormControl('',Validators.required),
-    categoria:new FormControl('',Validators.required),
-    imagen:new FormControl('',Validators.required),
-    alt:new FormControl('',Validators.required)
-    
+  producto = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    precio: new FormControl(0, Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required),
+    alt: new FormControl('', Validators.required)
+
   })
 
   constructor(public servicioCrud: CrudService) { }
+
+  ngOnInit(): void { }
+
+  async agregarProducto() {
+    if (this.producto.valid) {
+      let nuevoProducto: Producto = {
+        idProducto: '',
+        nombre: this.producto.value.nombre!,
+        precio: this.producto.value.precio!,
+        descripcion: this.producto.value.descripcion!,
+        categoria: this.producto.value.categoria!,
+        imagen: this.producto.value.imagen!,
+        alt: this.producto.value.alt!
+      }
+
+      await this.servicioCrud.crearProducto(nuevoProducto)
+        .then(producto => {
+          alert("Ha agregado un nuevo producto con exito")
+        })
+        .catch(error => {
+          alert("Ha ocurrido un error al cargar un producto")
+
+        })
+    };
+  }
 
 }
